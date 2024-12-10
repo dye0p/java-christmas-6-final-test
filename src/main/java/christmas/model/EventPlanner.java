af89c8b8961f5b5207e5d3be1b1c0793356c9068
DongYeop <s01054830455@gmail.com>
2024-12-10 17:08:22 +0900

    feat: 혜택을 계산하는 책임을 BenefitManager로 이동

--- src/main/java/christmas/model/EventPlanner.java
+++ src/main/java/christmas/model/EventPlanner.java
@@ -1,71 +1,22 @@
 package christmas.model;
 
+import christmas.model.benefit.BenefitManager;
+import java.util.Map;
+
 public class EventPlanner {
 
     private final Date date;
     private final Orders orders;
+    private final BenefitManager benefitManager;
 
-    public EventPlanner(Date date, Orders orders) {
+    public EventPlanner(Date date, Orders orders, BenefitManager benefitManager) {
         this.date = date;
         this.orders = orders;
+        this.benefitManager = benefitManager;
     }
 
-    public int canChristmasEvent() {
-        int discount = 0;
-        if (date.canChristmasEvent()) {
-            //1,000원으로 시작하여 크리스마스가 다가올수록 날마다 할인 금액이 100원씩 증가
-            discount += 1000;
-            for (int i = 1; i < date.getDate(); i++) {
-                discount += 100;
-            }
-        }
-        return discount;
-    }
-
-    public int canWeekdayEvent() {
-        int discount = 0;
-        if (date.isWeekday()) {
-            //평일 할인(일요일~목요일): 평일에는 디저트 메뉴를 메뉴 1개당 2,023원 할인
-            int dessertQuantity = 0;
-            for (Order order : orders.getOrders()) {
-                dessertQuantity += order.calculateDessertQuantity();
-            }
-            discount = dessertQuantity * 2023;
-        }
-        return discount;
-    }
-
-    public int canWeekendEvent() {
-        int discount = 0;
-        if (date.isWeekend()) {
-            //주말 할인(금요일, 토요일): 주말에는 메인 메뉴를 메뉴 1개당 2,023원 할인
-            int mainQuantity = 0;
-            for (Order order : orders.getOrders()) {
-                mainQuantity += order.calculateMainQuantity();
-            }
-            discount = mainQuantity * 2023;
-        }
-        return discount;
-    }
-
-    public int canSpecialEvent() {
-        int discount = 0;
-        if (date.isSpecialDay()) {
-            //특별 할인: 이벤트 달력에 별이 있으면 총주문 금액에서 1,000원 할인
-            discount = 1000;
-        }
-        return discount;
-    }
-
-    public int canGiftEvent() {
-        long discount = 0;
-
-        String giftMenu = canGiftMenu();
-        if (!giftMenu.equals("없음")) {
-            discount = Menu.getGiftMenu();
-        }
-
-        return (int) discount;
+    public Map<String, Integer> getBenefitResult() {
+        return benefitManager.calculateBenefit(orders, date);
     }
 
     public String canGiftMenu() {
@@ -77,9 +28,7 @@ public class EventPlanner {
         return gift;
     }
 
-    public String getBadge() {
-        int totalBenefitPrice = calculateTotalBenefitPrice();
-
+    public String getBadge(int totalBenefitPrice) {
         String badge = "없음";
 
         if (totalBenefitPrice >= 5000 && totalBenefitPrice < 10000) {
@@ -93,18 +42,6 @@ public class EventPlanner {
         }
 
         return badge;
-    }
-
-    public int calculateTotalDiscount() {
-        return canChristmasEvent() + canWeekdayEvent() + canWeekendEvent() + canSpecialEvent();
-    }
-
-    public int calculateTotalBenefitPrice() {
-        return calculateTotalDiscount() + canGiftEvent();
-    }
-
-    public int calculateAfterDiscountPrice() {
-        return orders.calculateBeforeDiscountTotalPrice() - calculateTotalDiscount();
     }
 
     public boolean isPossibleEvent() {
